456a15077c5359826a5cc569010de1449efc7065
Md Siyam <mdsiyam9424@gmail.com>
2024-12-20 22:28:24 +0600

    Job portal server is ready

---
+++ index.js
@@ -0,0 +1,206 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 6500;
+require('dotenv').config()
+
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://job-portal-ab6b1.web.app',
+    'https://job-portal-ab6b1.firebaseapp.com'
+
+  ],
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser())
+
+// const verifyToken = (req, res, next) => {
+//   const token = req?.cookies?.token
+//   if (!token) {
+//     return res.status(401).send({ message: 'Unauthorise access!' })
+//   }
+//   jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: 'Unauthorise access!' })
+//     }
+//     req.user = decoded;
+//     next()
+//   })
+// }
+const verifyToken = (req, res, next) => {
+  // console.log('biskut khamu',req?.cookies);
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorise access!" })
+  }
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorise access!" })
+    }
+    req.user = decoded;
+    next()
+  })
+}
+
+// DB_USER= job-master
+// DB_PASS= 1zONLoXjPZ2Posj2
+
+
+app.get('/', (req, res) => {
+  res.send('This is Job portal server')
+});
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttcu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const jobsCollection = client.db('jobPortal').collection('jobs')
+    // Applicant server
+    const jobApplicationCollection = client.db('jobPortal').collection('job_applications')
+
+
+    // Jwt token
+    // app.post('/jwt', async(req, res)=>{
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.SECRET_TOKEN, {expiresIn: "1h"})
+    //   res
+    //   .cookie('token', token, {
+    //     httpOnly:  true,
+    //     secure: false,
+
+    //   })
+    //   .send({success: true})
+    // })
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '5h' })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+      })
+        .send({ success: true })
+    })
+    app.post('/jwtLogout', async (req, res) => {
+      res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+      })
+        .send({ success: true })
+    })
+
+    app.get('/jobs', async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email }
+      }
+      const cursor = jobsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+
+      res.send(result)
+    })
+
+    app.post('/jobs', async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result)
+    })
+
+
+
+
+
+
+    // Application Collection
+
+    // get application data
+    app.get('/job-application', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const query = { applicant_email: email };
+
+      // if (req.user.email !== email) {
+      //   return res.status(403).send({ message: "Forbidden access!" })
+      // }
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send('Forbidden')
+      }
+
+      const result = await jobApplicationCollection.find(query).toArray();
+
+
+
+      for (const application of result) {
+
+        const query1 = { _id: new ObjectId(application.job_id) }
+        const job = await jobsCollection.findOne(query1)
+        if (job) {
+          application.title = job.title
+          application.job_id = job.job_id
+          application.company = job.company
+          application.company_logo = job.company_logo
+          application.location = job.location
+          application.requirements = job.requirements
+        }
+
+      }
+
+      res.send(result)
+    })
+
+    app.get('/job-application/jobs/:job_id', async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId }
+      const result = await jobApplicationCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/job-applications', async (req, res) => {
+      const cursor = req.body;
+      const result = await jobApplicationCollection.insertOne(cursor);
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`port is running on port : ${port}`)
+})
